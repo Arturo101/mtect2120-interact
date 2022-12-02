4015c9cdd94ea7b1efa36b9505f455476aa21083
James Lee <James.Lee@mail.citytech.cuny.edu>
2022-12-02 14:40:15 -0500

    particles

--- mtect2120-interact/Assets/AudioDetection.cs
+++ mtect2120-interact/Assets/AudioDetection.cs
@@ -15,29 +15,30 @@ public class AudioDetection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void MicrophoneToAudioClip()
     {
         string microphoneName = Microphone.devices[0];
-        microphoneClip = Microphone.Start(microphoneName, true, 20,AudioSettings.outputSampleRate);
+        microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
     }
 
-    public float GetLoudnessFromMicrophone(){
+    public float GetLoudnessFromMicrophone()
+    {
         return GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
     }
     public float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip)
     {
         int startPosition = clipPosition - SampleWindow;
 
-        if(startPosition < 0)
-        return 0;
+        if (startPosition < 0)
+            return 0;
 
         float[] waveData = new float[SampleWindow];
         clip.GetData(waveData, startPosition);
 
         float totalLoudness = 0;
-        for(int i = 0; i <SampleWindow; i++)
+        for (int i = 0; i < SampleWindow; i++)
         {
             totalLoudness += Mathf.Abs(waveData[i]);
         }
